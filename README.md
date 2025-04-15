74178ff200d2b61120d3d24a4d1c178056d366d0
Aditi Juneja <1509aditi@gmail.com>
2025-04-15 16:29:49 +0530

    minor tweaks

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # skimage backend - just for fun
+
+```
+python3 -m venv dev-env
+source dev-env/bin/activate
+pip install git+https://github.com/Schefflera-Arboricola/scikit-image.git@patch-1
+pip install -e .
+pip install git+https://github.com/betatim/scikit-image-backend-phony.git@main
+python3 rough.py
+```
--- rough.py
+++ rough.py
@@ -2,11 +2,37 @@ import skimage as ski
 import numpy as np
 import os
 
-os.environ["SKIMAGE_BACKENDS"] = "j4f"
+os.environ["SKIMAGE_DISPATCHING"] = "True"
 
 from importlib.metadata import entry_points
 
 print(entry_points(group="skimage_backends"))
 print(entry_points(group="skimage_backend_infos"))
 
-print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2]), x=7))
+os.environ["SKIMAGE_BACKEND_PRIORITY"] = "fake"
+print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2])))
+
+print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2]), x=5))
+
+os.environ["SKIMAGE_BACKEND_PRIORITY"] = "phony, j4f"
+
+print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2])))
+
+"""
+os.environ["SKIMAGE_BACKEND_PRIORITY"] = "j4f"
+print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2]), 5))
+
+print(ski.util._backends.get_skimage_backends())
+x = os.environ.get("SKIMAGE_DISPATCHING", False)
+print(x)
+
+print(ski.util._backends.all_backends_with_eps_combined())
+
+n = ski.metrics.mean_squared_error.__name__
+print(n)
+print(ski.util._backends.public_api_name(ski.metrics.mean_squared_error))
+
+os.environ["SKIMAGE_DISPATCHING"] = "False"
+print((ski.util._backends.get_skimage_backends()))
+print(ski.metrics.mean_squared_error(np.array([1, 2]), np.array([1, 2])))
+"""
